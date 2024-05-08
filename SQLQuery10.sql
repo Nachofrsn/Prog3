aa67c3803472936adb29552a69a3a913425368da
Nachofrsn <icordero@frsn.utn.edu.ar>
2024-05-08 15:57:16 -0300

    clase 8/5

---
+++ SQLQuery10.sql
@@ -0,0 +1,4 @@
+--insert into localidades (localidad, codigopostal) values ('Junin', '4000')
+--delete from localidades where idlocalidad = 15
+--update localidades set codigopostal = '4008' where idlocalidad = 9
+select * from localidades where codigopostal = '2900'
